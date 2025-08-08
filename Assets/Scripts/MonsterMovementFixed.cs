fbaf07f8f89ae313738eb5d72479fdf9f13edcd2
kohei sano <nlee9337@gmail.com>
2025-08-09 02:49:09 +0900

    Rename MonsterMovement class to MonsterMovementFixed for clarity

--- Assets/Scripts/MonsterMovementFixed.cs
+++ Assets/Scripts/MonsterMovementFixed.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class MonsterMovement : MonoBehaviour
+public class MonsterMovementFixed : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 3f;
